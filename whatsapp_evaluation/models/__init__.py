ec176ce486be33b536b74c986a597295442ea088
abdulazuz <abdulazuz716111@gmail.com>
2026-01-10 02:05:01 +0300

    adding the whatsapp_sale

--- whatsapp_evaluation/models/__init__.py
+++ whatsapp_evaluation/models/__init__.py
@@ -2,6 +2,6 @@ from . import whatsapp_account
 from . import whatsapp_message
 from . import discuss_channel
 from . import mail_message
-from . import whatsapp_template
 from . import whatsapp_template_variable
+from . import whatsapp_template
 from . import mail_thread

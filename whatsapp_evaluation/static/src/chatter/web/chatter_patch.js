a214185743bac5e84ffe9b4ce7ce8687b5627369
abdulazuz <abdulazuz716111@gmail.com>
2026-01-29 20:25:38 +0300

    adding the whatsapp_evaluation

--- whatsapp_evaluation/static/src/chatter/web/chatter_patch.js
+++ whatsapp_evaluation/static/src/chatter/web/chatter_patch.js
@@ -1,6 +1,6 @@
 /** @odoo-module */
 
-import { Chatter } from "@mail/chatter/web_portal/chatter";
+import { Chatter } from "@mail/core/web/chatter";
 import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 

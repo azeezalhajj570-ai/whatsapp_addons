ad54b76339dbb90dd72a0026a4c68b5ed97e9eef
abdulazuz <abdulazuz716111@gmail.com>
2026-02-05 21:27:48 +0300

    Add global OpenRouter chat wizard using Discuss AI chat

--- openrouterai_connector/__manifest__.py
+++ openrouterai_connector/__manifest__.py
@@ -17,6 +17,7 @@
         'views/openrouter_provider_views.xml',
         'views/openrouter_company_views.xml',
         'views/openrouter_model_views.xml',
+        'views/openrouter_chat_wizard_views.xml',
         'views/openrouter_sync_wizard_views.xml',
         'views/openrouter_menu.xml',
     ],
